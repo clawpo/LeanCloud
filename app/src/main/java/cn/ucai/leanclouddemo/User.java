a912c9ad217caa291579f6dadf17a7b704f792d3
耿小泰 <clawpo@foxmail.com>
2016-11-22 16:54:03 +0800

    添加用户实体类,包含昵称,性别和地理位置的字段,集成AVUser

---
+++ app/src/main/java/cn/ucai/leanclouddemo/User.java
@@ -0,0 +1,38 @@
+package cn.ucai.leanclouddemo;
+
+import com.avos.avoscloud.AVGeoPoint;
+import com.avos.avoscloud.AVUser;
+
+/**
+ * Created by clawpo on 2016/11/22.
+ */
+
+public class User extends AVUser {
+    private static final String USER_NICK = "user_nick";
+    private static final String USER_SEX = "user_sex";
+    private static final String USER_POINT = "user_point";
+
+    public String getUserNick(){
+        return getString(USER_NICK);
+    }
+
+    public void setUserNick(String nick){
+        put(USER_NICK,nick);
+    }
+
+    public int getUserSex(){
+        return getInt(USER_SEX);
+    }
+
+    public void setUserSex(int sex){
+        put(USER_SEX,sex);
+    }
+
+    public AVGeoPoint getUserPoint(){
+        return getAVGeoPoint(USER_POINT);
+    }
+
+    public void setUserPoint(AVGeoPoint point){
+        put(USER_POINT,point);
+    }
+}
