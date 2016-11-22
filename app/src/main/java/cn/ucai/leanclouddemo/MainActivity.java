6590cb02adf66a9b98894d728123e8be2f266f91
耿小泰 <clawpo@foxmail.com>
2016-11-22 18:00:37 +0800

    添加百度定位

--- app/src/main/java/cn/ucai/leanclouddemo/MainActivity.java
+++ app/src/main/java/cn/ucai/leanclouddemo/MainActivity.java
@@ -1,14 +1,27 @@
 package cn.ucai.leanclouddemo;
 
+import android.Manifest;
+import android.annotation.TargetApi;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.ListView;
 
 import com.avos.avoscloud.AVException;
 import com.avos.avoscloud.AVObject;
 import com.avos.avoscloud.SaveCallback;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    private final int SDK_PERMISSION_REQUEST = 127;
+    private ListView FunctionList;
+    private String permissionInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,5 +39,68 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+        // after andrioid m,must request Permiision on runtime
+        getPersimmions();
+    }
+
+    @TargetApi(23)
+    private void getPersimmions() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            ArrayList<String> permissions = new ArrayList<String>();
+            /***
+             * 定位权限为必须权限，用户如果禁止，则每次进入都会申请
+             */
+            // 定位精确位置
+            if(checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+                permissions.add(Manifest.permission.ACCESS_FINE_LOCATION);
+            }
+            if(checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+                permissions.add(Manifest.permission.ACCESS_COARSE_LOCATION);
+            }
+			/*
+			 * 读写权限和电话状态权限非必要权限(建议授予)只会申请一次，用户同意或者禁止，只会弹一次
+			 */
+            // 读写权限
+            if (addPermission(permissions, Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+                permissionInfo += "Manifest.permission.WRITE_EXTERNAL_STORAGE Deny \n";
+            }
+            // 读取电话状态权限
+            if (addPermission(permissions, Manifest.permission.READ_PHONE_STATE)) {
+                permissionInfo += "Manifest.permission.READ_PHONE_STATE Deny \n";
+            }
+
+            if (permissions.size() > 0) {
+                requestPermissions(permissions.toArray(new String[permissions.size()]), SDK_PERMISSION_REQUEST);
+            }
+        }
+    }
+
+    @TargetApi(23)
+    private boolean addPermission(ArrayList<String> permissionsList, String permission) {
+        if (checkSelfPermission(permission) != PackageManager.PERMISSION_GRANTED) { // 如果应用没有获得对应权限,则添加到列表中,准备批量申请
+            if (shouldShowRequestPermissionRationale(permission)){
+                return true;
+            }else{
+                permissionsList.add(permission);
+                return false;
+            }
+
+        }else{
+            return true;
+        }
+    }
+
+    @TargetApi(23)
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        // TODO Auto-generated method stub
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+    }
+
+    public void onClick(View view){
+        Intent intent = new Intent(MainActivity.this, LocationActivity.class);
+        intent.putExtra("from", 0);
+        startActivity(intent);
     }
 }
--- app/src/main/java/cn/ucai/leanclouddemo/MyLeanCloudApp.java
+++ app/src/main/java/cn/ucai/leanclouddemo/MyLeanCloudApp.java
@@ -1,19 +1,31 @@
 package cn.ucai.leanclouddemo;
 
 import android.app.Application;
+import android.app.Service;
+import android.os.Vibrator;
 
 import com.avos.avoscloud.AVOSCloud;
+import com.baidu.mapapi.SDKInitializer;
 
 /**
  * Created by clawpo on 2016/11/22.
  */
 
 public class MyLeanCloudApp extends Application {
+    public LocationService locationService;
+    public Vibrator mVibrator;
     @Override
     public void onCreate() {
         super.onCreate();
 
         // 初始化参数依次为 this, AppId, AppKey
         AVOSCloud.initialize(this,"5FAl4q4NdF3OhFy2Vibg7IVa-gzGzoHsz","6QpR4hJnFdLu6QOQDVf9q0t9");
+        /***
+         * 初始化定位sdk，建议在Application中创建
+         */
+        locationService = new LocationService(getApplicationContext());
+        mVibrator =(Vibrator)getApplicationContext().getSystemService(Service.VIBRATOR_SERVICE);
+        SDKInitializer.initialize(getApplicationContext());
+
     }
 }
